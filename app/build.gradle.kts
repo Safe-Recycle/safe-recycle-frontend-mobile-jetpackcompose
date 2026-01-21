a91e07ea3ae831444cd265d4f5bc2be4fe3e5c46
MuhammadAndra <m.andradzaki@gmail.com>
2026-01-21 18:39:54 +0700

    [add] done setup global font

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+
+//    kotlin("plugin.serialization") version "2.0.21"
+//    id("com.google.devtools.ksp")
+//    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -46,6 +50,36 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    //fonts
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.10.1")
+
+    //lucide (icon)
+    implementation("com.composables:icons-lucide-android:1.0.0")
+
+    //compose navigation
+    implementation("androidx.navigation:navigation-compose:2.8.9")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
+
+    //retrofit (HTTP connection)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+    //coil (load image)
+    implementation("io.coil-kt.coil3:coil-compose:3.2.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.2.0")
+
+    //datastore (store auth token)
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    //hilt (dependency injection)
+//    implementation("com.google.dagger:hilt-android:2.59")
+//    ksp("com.google.dagger:hilt-compiler:2.59")
+//    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/saferecycle/MainActivity.kt
+++ app/src/main/java/com/example/saferecycle/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.composables.icons.lucide.Activity
 import com.example.saferecycle.ui.theme.SafeRecycleTheme
+import com.example.saferecycle.ui.theme.fontFamily
+import com.composables.icons.lucide.Lucide
+import com.composables.icons.lucide.ScanSearch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +28,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             SafeRecycleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Column(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        Text(
+                            text = "Whereas recognition of the inherent dignity",
+                            fontFamily = fontFamily,
+                            fontWeight = FontWeight.Normal
+                        )
+                        Text(
+                            text = "Whereas recognition of the inherent dignity",
+                            fontFamily = fontFamily,
+                            fontWeight = FontWeight.Medium
+                        )
+                        Text(
+                            text = "Whereas recognition of the inherent dignity",
+                            fontFamily = fontFamily,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Icon(imageVector = Lucide.ScanSearch, contentDescription = "lucide icon")
+                    }
                 }
             }
         }
@@ -34,6 +58,9 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        fontFamily = fontFamily,
+//        fontWeight = FontWeight.Bold,
+        fontSize = 50.sp,
         modifier = modifier
     )
 }
